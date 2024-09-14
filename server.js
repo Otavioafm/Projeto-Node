ce97745271fb0249a2739f411cd3342d2edb4f72
Otavioafm <otaviofurtaf@gmail.com>
2024-09-14 14:41:19 -0300

    Atualizei dependências e fiz ajustes no servidor

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 import express  from "express";
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 const user=[]
 
 const app=express()
 app.use(express.json())
 
-app.post('/usuarios',(req, res)=>{
-    user.push(req.body)
-    res.send("Ok,Post")
-    res.status(2001).json(req.body)
+app.post('/usuarios',async (req, res) => {
+   await prisma.user.create({
+        data:{
+            email:req.body.email,
+            name:req.body.name,
+            age:req.body.age
+        }
+    })
+
+    res.status(201).json(req.body) 
 })
 
 app.get('/usuarios',(req, res)=>{
